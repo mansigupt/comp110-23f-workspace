9b1cc6bbbf61bf513b09b6be5a8bffad7a720f2f
Mansi Gupta <mansig@email.unc.edu>
2023-09-16 11:08:51 -0400

    EX02 Changes

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -4,7 +4,7 @@ __author__ = "730391057"
 answer: str = "python"
 guess: str = input(f"What is your {len(answer)}-letter guess? ")
 word_idx: int = 0
-emoji: str = ""
+box: str = ""
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
@@ -13,12 +13,19 @@ YELLOW_BOX: str = "\U0001F7E8"
 while len(guess) != len(answer):
     guess = input(f"That was not {len(answer)} letters! Try again: ")
 
-while len(guess) < len(answer): 
-    if guess[word_idx] == answer[word_idx]:
-        emoji += (GREEN_BOX)
-        print(str(emoji))
+while word_idx < len(answer):
+    if word_idx < len(guess) and guess[word_idx] == answer[word_idx]:
+        box += GREEN_BOX
+    else:
+        box += WHITE_BOX
+    word_idx += 1
+
+print(box)
 
 if guess != answer:
     print("Not quite. Play again soon! ")
+
+if guess == answer:
+    print("Woo! You got it! ")
 
     
