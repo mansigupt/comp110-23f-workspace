4e0d8d5a0c69967b457a8fd6bfb3a4f5fedb4f15
Mansi Gupta <mansig@email.unc.edu>
2023-09-16 11:21:52 -0400

    Finish EX02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -10,14 +10,29 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
+# Ensure the guessed word has the same length as the answer
 while len(guess) != len(answer):
     guess = input(f"That was not {len(answer)} letters! Try again: ")
 
 while word_idx < len(answer):
     if word_idx < len(guess) and guess[word_idx] == answer[word_idx]:
+        # Insert green box for correct character and position
         box += GREEN_BOX
     else:
-        box += WHITE_BOX
+        # Initialize boolean and alternate index variables
+        diff_place = False
+        alt_index: int = 0
+        # While loop to check for matches elsewhere in the secret word
+        while not diff_place and alt_index < len(answer):
+            if diff_place != word_idx and guess[word_idx] == answer[alt_index]:
+                diff_place = True
+            alt_index += 1
+        if diff_place:
+            # Add a yellow box for correct character but wrong position
+            box += YELLOW_BOX
+        else: 
+            # Add a white box for incorrect character
+            box += WHITE_BOX
     word_idx += 1
 
 print(box)
