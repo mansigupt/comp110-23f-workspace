3878932092c90e6e63b84874656b8350d6510626
Mansi Gupta <mansig@email.unc.edu>
2023-09-21 14:03:18 -0400

    starting ex03

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -1,3 +1,11 @@
 """EX03 - Structured Wordle!"""
 __author__ = "730391057"
 
+def contains_char(search_word: str, find_char: str)-> bool:
+    """Determining if a single character is part of the word."""
+    assert len(find_char) == 1
+    word_idx: int = 0
+    if search_word[word_idx] == find_char:
+        return True
+    else:
+        return False  
