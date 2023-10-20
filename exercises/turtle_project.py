e36a3f032941376237ce9adfade2fd0ba4fe986a
Mansi Gupta <mansig@email.unc.edu>
2023-10-20 12:15:35 -0400

    turtle finish

--- exercises/turtle_project.py
+++ exercises/turtle_project.py
@@ -1,4 +1,4 @@
-"""EX05 - Turtle!"""
+"""EX05 - Turtle- Mountain scene with random snowflake presentation!"""
 __author__ = "730391057"
 
 from turtle import Turtle, Screen, colormode, done
@@ -12,8 +12,10 @@ colormode(255)
 window = Screen()
 window.bgcolor(173, 216, 230)  # "lightblue" in RGB (173, 216, 230)
 
+
 # Function to draw a mountain
-def draw_mountain(x, y, width, height, color):
+def draw_mountain(x: int, y: int, width: int, height: int, color: tuple[int, int, int]) -> None:
+    """Simplified function to draw a mountain."""
     scene.penup()
     scene.fillcolor(color)
     scene.goto(x, y)
@@ -30,7 +32,8 @@ def draw_mountain(x, y, width, height, color):
 
 
 # Function to draw the sun
-def draw_sun(x, y, radius):
+def draw_sun(x: int, y: int, radius: int) -> None:
+    """Simplified function to draw sun."""
     scene.penup()
     scene.fillcolor(255, 255, 0)
     scene.goto(x, y)
@@ -43,7 +46,8 @@ def draw_sun(x, y, radius):
 
 
 # Function to draw a cloud
-def draw_cloud(x, y):
+def draw_cloud(x: int, y: int) -> None:
+    """Simplified function to draw clouds."""
     scene.penup()
     scene.goto(x, y)
     scene.pendown()
@@ -51,7 +55,7 @@ def draw_cloud(x, y):
     scene.begin_fill()
     
     # Draw the cloud's body (three humps) 
-    scene.setheading(90) # Draws the cloud at a specific angle
+    scene.setheading(90)  # Draws the cloud at a specific angle
     cloud: int = 0 
     while cloud < 3:
         scene.circle(30, 180)
@@ -64,7 +68,8 @@ def draw_cloud(x, y):
     scene.forward(50)
 
 
-def draw_grass():
+def draw_grass() -> None:
+    """Function to draw grass."""
     scene.penup()
     scene.goto(-400, -350)  
     scene.pendown()
@@ -81,15 +86,17 @@ def draw_grass():
     
     scene.end_fill()
 
+
 # Function to draw snowflakes
-def draw_snowflake(x, y):
+def draw_snowflake(x: int, y: int) -> None:
+    """Function to draw snowflakes using random."""
     scene.penup()
     scene.goto(x, y)
     scene.pendown()
     scene.color(255, 255, 255)
     scene.begin_fill()
     
-    flake = 0  
+    flake: int = 0  
     while flake < 6:
         scene.forward(10)
         scene.backward(10)
@@ -98,14 +105,16 @@ def draw_snowflake(x, y):
     
     scene.end_fill()
 
+
 def main() -> None:
+    """Main function to draw the scene I coded above."""
     # Set the drawing speed
     scene.speed(0)
 
     # Draw mountains
-    draw_mountain(-300, -150, 200, 300, "gray")
-    draw_mountain(100, -150, 150, 250, "dimgray")
-    draw_mountain(-150, -150, 250, 400, "slategray")
+    draw_mountain(-300, -150, 200, 300, (128, 128, 128))  # "gray"
+    draw_mountain(100, -150, 150, 250, (105, 105, 105))  # "dimgray"
+    draw_mountain(-150, -150, 250, 400, (112, 128, 144))  # "slategray"
 
     # Draw the sun
     draw_sun(300, 300, 50)
@@ -119,12 +128,15 @@ def main() -> None:
     draw_grass()
 
     # Draw snowflakes
-    for _ in range(50):
+    snow_count: int = 0  # Initialize a counter variable
+    while snow_count < 50:
         x = random.randint(-400, 400)
         y = random.randint(0, 400)
         draw_snowflake(x, y)
+        snow_count += 1
 
-    done ()
+    done()
+
 
 if __name__ == "__main__":
     main()
