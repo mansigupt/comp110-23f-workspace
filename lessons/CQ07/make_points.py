a183910e125f348cde1926bf39ecc2eab8441c50
Mansi Gupta <mansig@email.unc.edu>
2023-11-02 10:38:28 -0400

    OOP

---
+++ lessons/CQ07/make_points.py
@@ -0,0 +1,5 @@
+from lessons.CQ07.point import Point
+
+my_point: Point = (10.0, 20.0)
+my_new_point: Point = my_point.scale(4)
+print(my_new_point.x)
---
+++ lessons/CQ07/point.py
@@ -0,0 +1,23 @@
+"""Challenge Question 7."""
+from __future__ import annotations
+__author__ = "730391057"
+
+
+class Point:
+
+    x: float
+    y: float
+
+    def __init__(self, x_init: float, y_init: float):
+        """My constructor"""
+        self.x = x_init
+        self.y = y_init
+    
+    def scale_by(self, factor: int) -> None:
+        """Increasing my number"""
+        self.x *= factor
+        self.y *= factor
+
+    def scale(self, factor: int) -> Point:
+        new_point: Point = Point(self.x * factor, self.y * factor)
+        return new_point
