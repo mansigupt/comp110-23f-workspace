824852964ff6c6fb8b048f7393df5859d402617c
Mansi Gupta <mansig@email.unc.edu>
2023-11-02 10:44:26 -0400

    finish oop

--- lessons/CQ07/make_points.py
+++ lessons/CQ07/make_points.py
@@ -1,5 +1,7 @@
+"""Method calling for points."""
+
 from lessons.CQ07.point import Point
 
-my_point: Point = (10.0, 20.0)
+my_point: Point = Point(10.0, 20.0)
 my_new_point: Point = my_point.scale(4)
-print(my_new_point.x)
+print(my_new_point.x, my_new_point.y)
--- lessons/CQ07/point.py
+++ lessons/CQ07/point.py
@@ -4,20 +4,21 @@ __author__ = "730391057"
 
 
 class Point:
-
+    """Making a new class called point."""
     x: float
     y: float
 
     def __init__(self, x_init: float, y_init: float):
-        """My constructor"""
+        """My constructor."""
         self.x = x_init
         self.y = y_init
     
     def scale_by(self, factor: int) -> None:
-        """Increasing my number"""
+        """Increasing my number."""
         self.x *= factor
         self.y *= factor
 
     def scale(self, factor: int) -> Point:
+        """Return a point."""
         new_point: Point = Point(self.x * factor, self.y * factor)
         return new_point
