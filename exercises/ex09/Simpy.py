3de8710f44552040f240be3f5fd5a2d316295699
Mansi Gupta <mansig@email.unc.edu>
2023-11-21 16:22:39 -0500

    simpy progress

--- exercises/ex09/Simpy.py
+++ exercises/ex09/Simpy.py
@@ -8,18 +8,22 @@ __author__ = "730391057"
 
 
 class Simpy:
+    """Utility class for numerical operations."""
     def __init__(self, values: list[float]) -> None:
+        """Constructor for Simpy."""
         self.values = values
 
     def __str__(self) -> str:
-        return (f"Simpy({self.values}")
+        """String representation of the Simpy object."""
+        return f"Simpy({self.values})"
     
     def fill(self, value: float, num_values: int) -> None:
-        self.values = value * num_values
-        return self
+        """Fill the Simpy object with a specified value for a given number of times."""
+        self.values = [value] * num_values
 
     def arange(self, start: float, stop: float, step: float = 1.0) -> None:
-        assert step != 0.0, "Step cannot be 0.0"
+        """Create a range of values from start to stop with a specified step."""
+        assert step != 0.0 
         self.values = []
         current_value = start
         while (step > 0 and current_value < stop) or (step < 0 and current_value > stop):
@@ -28,17 +32,21 @@ class Simpy:
         return self
         
     def sum(self) -> float:
+        """Calculate the sum of values in the Simpy object."""
         return sum(self.values)
     
     def __add__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Add a Simpy object or float to the current Simpy object."""
         if isinstance(rhs, Simpy):
-            return Simpy(self.values + rhs.values)
+            min_len = min(len(self.values), len(rhs.values))
+            result_values = [self.values[i] + rhs.values[i] for i in range(min_len)]
         elif isinstance(rhs, float):
-            return Simpy([x + rhs for x in self.values])
-        else:
-            raise TypeError(f"Unsupported type for addition: {type(rhs)}")
+            result_values = [x + rhs for x in self.values]
         
-    def __pow__(self, rhs: Union[float, Simpy]) -> 'Simpy':
+        return Simpy(result_values)
+        
+    def __pow__(self, rhs: Union[float, Simpy]) -> Simpy:
+        """Raise each element in the Simpy object to the power of the corresponding element in another Simpy object or float."""
         if isinstance(rhs, Simpy):
             result_values = [self.values[i] ** rhs.values[i] for i in range(len(self.values))]
         elif isinstance(rhs, float):
@@ -46,6 +54,7 @@ class Simpy:
         return Simpy(result_values)
     
     def __eq__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Compare the Simpy object with another Simpy object or float for equality."""
         if isinstance(rhs, Simpy):
             min_length = min(len(self.values), len(rhs.values))
             return [self.values[i] == rhs.values[i] for i in range(min_length)]
@@ -53,6 +62,7 @@ class Simpy:
             return [x == rhs for x in self.values]
         
     def __gt__(self, rhs: Union[float, Simpy]) -> list[bool]:
+        """Compare the Simpy object with another Simpy object or float for greater than."""
         if isinstance(rhs, Simpy):
             min_length = min(len(self.values), len(rhs.values))
             return [self.values[i] > rhs.values[i] for i in range(min_length)]
@@ -60,5 +70,6 @@ class Simpy:
             return [x > rhs for x in self.values]
         
     def __getitem__(self, rhs: Union[int, list[bool]]) -> Union[float, Simpy]:
+        """Retrieve an element or create a new Simpy object using indexing or boolean masking."""
         return self.values[rhs]
     
