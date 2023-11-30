02666496bab22b1124a9cd3611af10776f6f93cc
Mansi Gupta <mansig@email.unc.edu>
2023-11-30 14:10:17 -0500

    class

--- exercises/ex10/data_utils.py
+++ exercises/ex10/data_utils.py
@@ -1,5 +1,17 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730391057"
 
-# Define your functions below
+from io import TextIOWrapper
+import csv
+
+def read_csv_rows(filename: str) -> list[dict[str, str]]:
+    """Read an entire CSV of data into a list of rows, each row represented as a dictionary."""
+    rows: list[dict[str, str]] = []
+    file_handle: TextIOWrapper = open(filename, "r")
+    for line in file_handle:
+        line = line.strip()
+        rows.append(line)
+    file_handle.close()
+    return rows
+
